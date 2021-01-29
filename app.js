0946c3dd0c38e6953f800adacd609ddb9fb50d11
administrator <mkibanyu96@gmail.com>
2021-01-30 00:17:04 +0300

    full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Welcome to DevOps Mr. Kibanyu!'));
+app.get('/', (req, res) => res.send('Am done with ci/cd!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
